7777e8dd796884efbdbe37ada677efd294aa6ec4
Diego Torres Mijarra <diego.torres@alumno.iesluisvives.org>
2024-01-23 16:59:56 +0100

    feat(common database config & .env)

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import { ValidationPipe } from '@nestjs/common'
+import * as process from 'process'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
-  await app.listen(3000)
+  app.useGlobalPipes(new ValidationPipe())
+  await app.listen(process.env.API_PORT || 3000)
 }
 bootstrap()

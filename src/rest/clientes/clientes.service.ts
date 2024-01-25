73d093c37681126a4c4f03a9d280ec26594d3e83
jaime9lozano <jaime9lozano@gmail.com>
2024-01-25 18:02:55 +0100

    FindAll, FindOne servicio

--- src/rest/clientes/clientes.service.ts
+++ src/rest/clientes/clientes.service.ts
@@ -1,26 +1,107 @@
-import { Injectable } from '@nestjs/common'
+import { Inject, Injectable, Logger, NotFoundException } from '@nestjs/common'
 import { CreateClienteDto } from './dto/create-cliente.dto'
 import { UpdateClienteDto } from './dto/update-cliente.dto'
+import { ClienteMapper } from './mapper/cliente.mapper'
+import { InjectRepository } from '@nestjs/typeorm'
+import { Cliente } from './entities/cliente.entity'
+import { Repository } from 'typeorm'
+import { CACHE_MANAGER } from '@nestjs/cache-manager'
+import { Cache } from 'cache-manager'
+import {
+  FilterOperator,
+  FilterSuffix,
+  paginate,
+  PaginateQuery,
+} from 'nestjs-paginate'
+import { hash } from 'typeorm/util/StringUtils'
+import { ResponseCliente } from './dto/response-cliente.dto'
 
 @Injectable()
 export class ClientesService {
+  logger: Logger = new Logger(ClientesService.name)
+  constructor(
+    @InjectRepository(Cliente)
+    private readonly clienteRepository: Repository<Cliente>,
+    private readonly mapper: ClienteMapper,
+    @Inject(CACHE_MANAGER) private cacheManager: Cache,
+  ) {}
+  async findAll(query: PaginateQuery) {
+    this.logger.log('Buscando todos los clientes')
+    const cache = await this.cacheManager.get(
+      `all_clientes_page_${hash(JSON.stringify(query))}`,
+    )
+    if (cache) {
+      this.logger.log('Buscando todos los clientes en cache')
+      return cache
+    }
+    const pagination = await paginate(query, this.clienteRepository, {
+      sortableColumns: [
+        'dni',
+        'nombre',
+        'apellido',
+        'imagen',
+        'edad',
+        'telefono',
+      ],
+      defaultSortBy: [['id', 'ASC']],
+      searchableColumns: [
+        'dni',
+        'nombre',
+        'apellido',
+        'imagen',
+        'edad',
+        'telefono',
+      ],
+      filterableColumns: {
+        dni: [FilterOperator.EQ, FilterSuffix.NOT],
+        nombre: [FilterOperator.EQ, FilterSuffix.NOT],
+        apellido: [FilterOperator.EQ, FilterSuffix.NOT],
+        imagen: [FilterOperator.EQ, FilterSuffix.NOT],
+        edad: true,
+        telefono: true,
+        deleted: [FilterOperator.EQ, FilterSuffix.NOT],
+      },
+    })
+    const res = {
+      data: (pagination.data ?? []).map((cliente) =>
+        this.mapper.toResponse(cliente),
+      ),
+      meta: pagination.meta,
+      links: pagination.links,
+    }
+    await this.cacheManager.set(
+      `all_clientes_page_${hash(JSON.stringify(query))}`,
+      res,
+      60,
+    )
+    return res
+  }
+
+  async findOne(id: string) {
+    this.logger.log(`Buscando cliente con id ${id}`)
+    const cache: ResponseCliente = await this.cacheManager.get(`cliente_${id}`)
+    if (cache) {
+      this.logger.log(`Cliente con id ${id} encontrado en cache`)
+      return cache
+    }
+    const clienteEncontrar = await this.clienteRepository.findOneBy({ id })
+    if (!clienteEncontrar) {
+      throw new NotFoundException(`Cliente con id ${id} no encontrada`)
+    } else {
+      await this.cacheManager.set(`cliente_${id}`, clienteEncontrar, 60)
+      return clienteEncontrar
+    }
+  }
+
   create(createClienteDto: CreateClienteDto) {
     return 'This action adds a new cliente'
-  }
-
-  findAll() {
-    return `This action returns all clientes`
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} cliente`
   }
 
   update(id: number, updateClienteDto: UpdateClienteDto) {
     return `This action updates a #${id} cliente`
   }
 
-  remove(id: number) {
+  removeSoft(id: number) {
     return `This action removes a #${id} cliente`
   }
 }

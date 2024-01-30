13a816c43b825665b1a19c10e5c3c64f73f746f1
jaime9lozano <jaime9lozano@gmail.com>
2024-01-30 19:58:29 +0100

    Swagger

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule } from '@nestjs/config'
 import { DataBaseConfigModule } from './config/data-base-config/data-base-config.module'
 import { ClientesModule } from './rest/clientes/clientes.module'
 import { CacheModule } from '@nestjs/cache-manager'
+import { CorsConfigModule } from './config/cors/cors.module'
 
 @Module({
   imports: [
@@ -12,6 +13,7 @@ import { CacheModule } from '@nestjs/cache-manager'
       ignoreEnvFile: false,
     }),
     //bbdd con postgrest
+    CorsConfigModule,
     DataBaseConfigModule,
     CacheModule.register(),
     ClientesModule,
--- src/rest/clientes/clientes.controller.ts
+++ src/rest/clientes/clientes.controller.ts
@@ -20,15 +20,29 @@ import { ClientesService } from './clientes.service'
 import { CreateClienteDto } from './dto/create-cliente.dto'
 import { UpdateClienteDto } from './dto/update-cliente.dto'
 import { CacheInterceptor, CacheKey, CacheTTL } from '@nestjs/cache-manager'
-import { Paginate, PaginateQuery } from 'nestjs-paginate'
+import { Paginate, Paginated, PaginateQuery } from 'nestjs-paginate'
 import { clienteExistGuard } from './guard/cliente-exists.guard'
 import { diskStorage } from 'multer'
 import { FileInterceptor } from '@nestjs/platform-express'
 import { extname, parse } from 'path'
 import { Request } from 'express'
+import {
+  ApiBadRequestResponse,
+  ApiBearerAuth,
+  ApiBody,
+  ApiConsumes,
+  ApiNotFoundResponse,
+  ApiParam,
+  ApiProperty,
+  ApiQuery,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger'
+import { ResponseCliente } from './dto/response-cliente.dto'
 
 @Controller('clientes')
 @UseInterceptors(CacheInterceptor)
+@ApiTags('Clientes')
 export class ClientesController {
   logger: Logger = new Logger(ClientesController.name)
   constructor(private readonly clientesService: ClientesService) {}
@@ -36,11 +50,63 @@ export class ClientesController {
   @Get()
   @CacheKey('all_clientes')
   @CacheTTL(30)
+  @ApiResponse({
+    status: 200,
+    description:
+      'Lista de clientes paginada. Se puede filtrar por limite, pagina sortBy, filter y search',
+    type: Paginated<ResponseCliente>,
+  })
+  @ApiQuery({
+    description: 'Filtro por limite por pagina',
+    name: 'limit',
+    required: false,
+    type: Number,
+  })
+  @ApiQuery({
+    description: 'Filtro por pagina',
+    name: 'page',
+    required: false,
+    type: Number,
+  })
+  @ApiQuery({
+    description: 'Filtro de ordenación: campo:ASC|DESC',
+    name: 'sortBy',
+    required: false,
+    type: String,
+  })
+  @ApiQuery({
+    description: 'Filtro de busqueda: filter.campo = $eq:valor',
+    name: 'filter',
+    required: false,
+    type: String,
+  })
+  @ApiQuery({
+    description: 'Filtro de busqueda: search = valor',
+    name: 'search',
+    required: false,
+    type: String,
+  })
   async findAll(@Paginate() query: PaginateQuery) {
     this.logger.log('Buscando todos los clientes')
     return await this.clientesService.findAll(query)
   }
 
+  @ApiResponse({
+    status: 200,
+    description: 'Cliente encontrado',
+    type: ResponseCliente,
+  })
+  @ApiParam({
+    name: 'id',
+    description: 'Identificador del cliente',
+    type: Number,
+  })
+  @ApiNotFoundResponse({
+    description: 'Cliente no encontrado',
+  })
+  @ApiBadRequestResponse({
+    description: 'El id del cliente no es válido',
+  })
   @Get(':id')
   findOne(@Param('id', ParseUUIDPipe) id: string) {
     this.logger.log(`Buscando cliente con id ${id}`)
@@ -49,12 +115,48 @@ export class ClientesController {
 
   @Post()
   @HttpCode(201)
+  @ApiBearerAuth() // Indicar que se requiere autenticación con JWT en Swagger
+  @ApiResponse({
+    status: 201,
+    description: 'Cliente creado',
+    type: ResponseCliente,
+  })
+  @ApiBody({
+    description: 'Datos del cliente a crear',
+    type: CreateClienteDto,
+  })
+  @ApiBadRequestResponse({
+    description:
+      'Algunos de los campos no son válidos según la especificación del DTO',
+  })
   create(@Body() createClienteDto: CreateClienteDto) {
     this.logger.log('Creando un nuevo cliente')
     return this.clientesService.create(createClienteDto)
   }
 
   @Put(':id')
+  @ApiBearerAuth() // Indicar que se requiere autenticación con JWT en Swagger
+  @ApiResponse({
+    status: 200,
+    description: 'Cliente actualizado',
+    type: ResponseCliente,
+  })
+  @ApiParam({
+    name: 'id',
+    description: 'Identificador del cliente',
+    type: Number,
+  })
+  @ApiBody({
+    description: 'Datos del cliente a actualizar',
+    type: UpdateClienteDto,
+  })
+  @ApiNotFoundResponse({
+    description: 'Cliente no encontrado',
+  })
+  @ApiBadRequestResponse({
+    description:
+      'Algunos de los campos no son válidos según la especificación del DTO',
+  })
   update(
     @Param('id', ParseUUIDPipe) id: string,
     @Body() updateClienteDto: UpdateClienteDto,
@@ -65,12 +167,62 @@ export class ClientesController {
 
   @Delete(':id')
   @HttpCode(204)
+  @ApiBearerAuth() // Indicar que se requiere autenticación con JWT en Swagger
+  @ApiResponse({
+    status: 204,
+    description: 'Cliente eliminado',
+  })
+  @ApiParam({
+    name: 'id',
+    description: 'Identificador del cliente',
+    type: Number,
+  })
+  @ApiNotFoundResponse({
+    description: 'Cliente no encontrado',
+  })
+  @ApiBadRequestResponse({
+    description: 'El id del cliente no es válido',
+  })
   remove(@Param('id', ParseUUIDPipe) id: string) {
     this.logger.log(`Eliminando cliente con id ${id}`)
     return this.clientesService.removeSoft(id)
   }
   @Patch('/imagen/:id')
   @UseGuards(clienteExistGuard)
+  @ApiBearerAuth() // Indicar que se requiere autenticación con JWT en Swagger
+  @ApiResponse({
+    status: 200,
+    description: 'Imagen actualizada',
+    type: ResponseCliente,
+  })
+  @ApiParam({
+    name: 'id',
+    description: 'Identificador del cliente',
+    type: Number,
+  })
+  @ApiProperty({
+    name: 'file',
+    description: 'Fichero de imagen',
+    type: 'string',
+    format: 'binary',
+  })
+  @ApiConsumes('multipart/form-data')
+  @ApiBody({
+    description: 'Fichero de imagen',
+    type: FileInterceptor('file'),
+  })
+  @ApiNotFoundResponse({
+    description: 'Cliente no encontrado',
+  })
+  @ApiBadRequestResponse({
+    description: 'El id del cliente no es válido',
+  })
+  @ApiBadRequestResponse({
+    description: 'El fichero no es válido o de un tipo no soportado',
+  })
+  @ApiBadRequestResponse({
+    description: 'El fichero no puede ser mayor a 1 megabyte',
+  })
   @UseInterceptors(
     FileInterceptor('file', {
       storage: diskStorage({

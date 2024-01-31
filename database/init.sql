3f749584e058e6394dc0b9245c2506a777343818
Diego Torres Mijarra <diego.torres@alumno.iesluisvives.org>
2024-01-31 19:44:32 +0100

    fix(updated init.sql):
    deleted duplicated

--- database/init.sql
+++ database/init.sql
@@ -7,6 +7,8 @@ DROP TABLE IF EXISTS "trabajadores";
 DROP TABLE IF EXISTS "clientes";
 DROP TABLE IF EXISTS "proveedores";
 DROP SEQUENCE IF EXISTS proveedores_id_seq;
+DROP TABLE IF EXISTS "restaurantes";
+DROP SEQUENCE IF EXISTS restaurantes_id_seq;
 -- Crear la tabla position
 CREATE TABLE "public"."posiciones" (
                                        "id" uuid DEFAULT uuid_generate_v4() NOT NULL,
@@ -93,3 +95,25 @@ CREATE TABLE "public"."proveedores" (
                                    "deleted" BOOLEAN DEFAULT true,
                                    CONSTRAINT "proveedores_pk" PRIMARY KEY ("id")
 )WITH (oids = false);
+
+
+--Crear sequencia para la tabla RESTAURANTES
+CREATE SEQUENCE restaurantes_id_seq INCREMENT 1 MINVALUE 4 MAXVALUE 100 CACHE 1;
+-- Crear la tabla RESTAURANTES
+CREATE TABLE "public"."restaurantes" (
+                                         "id" bigint DEFAULT nextval('restaurantes_id_seq') NOT NULL,
+                                         "nombre" character varying(255) UNIQUE NOT NULL,
+                                         "calle" character varying(255) NOT NULL,
+                                         "localidad" character varying(255) NOT NULL,
+                                         "capacidad" integer NOT NULL,
+                                         "borrado" boolean DEFAULT false NOT NULL,
+                                         "creado_en" timestamp DEFAULT now() NOT NULL,
+                                         "actualizado_en" timestamp DEFAULT now() NOT NULL,
+                                         CONSTRAINT "restaurantes_pkey" PRIMARY KEY ("id")
+) WITH (oids = false);
+-- Insertar datos en la tabla RESTAURANTES
+INSERT INTO "restaurantes" ("id", "nombre", "calle", "localidad", "capacidad", "borrado",  "creado_en", "actualizado_en")
+VALUES
+    (1, 'madre nodriza', 'espacial', 'madrid', 200 , false, '1975-12-12', '2022-12-12'),
+    (2, 'primer hijo', 'veloz', 'getafe',  80, false,  '2004-12-12', '2022-12-12'),
+    (3, 'el abuelo', 'fifrancisco', 'toledo', 120, true,  '1936-12-12', '2022-12-12');

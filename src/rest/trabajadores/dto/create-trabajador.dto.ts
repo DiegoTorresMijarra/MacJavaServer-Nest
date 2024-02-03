a24c7d723ae0fc150f45d97a26144dc1a1bf43e6
Diego Torres Mijarra <diego.torres@alumno.iesluisvives.org>
2024-02-03 09:58:35 +0100

    feat(doc dtos)

--- src/rest/trabajadores/dto/create-trabajador.dto.ts
+++ src/rest/trabajadores/dto/create-trabajador.dto.ts
@@ -9,29 +9,50 @@ import {
   Min,
 } from 'class-validator'
 import { PosicionesValidas } from '../../posiciones/entities/posicion.entity'
+import { ApiProperty } from '@nestjs/swagger'
 
 export class CreateTrabajadorDto {
+  @ApiProperty({
+    example: '64857416T',
+    description: 'Dni del trabajador',
+  })
   @IsNotEmpty({ message: 'El DNI no debe estar vacio' })
   @IsIdentityCard('ES', {
     message: 'El DNI debe ser un DNI valido',
   })
   dni: string
 
+  @ApiProperty({
+    example: 'Rodrigo',
+    description: 'El nombre del trabajador',
+  })
   @IsNotEmpty({ message: 'El nombre no debe estar vacio' })
   @IsString({ message: 'El nombre debe ser un string' })
   @Matches(/\S+/g) //que no tenga espacios en blanco
   nombre: string
 
+  @ApiProperty({
+    example: 'Lopez',
+    description: 'El apellido del trabajador',
+  })
   @IsNotEmpty({ message: 'El nombre no debe estar vacio' })
   @IsString({ message: 'El nombre debe ser un string' })
   @Matches(/\S+/g) //que no tenga espacios en blanco
   apellido: string
 
+  @ApiProperty({
+    example: 25,
+    description: 'La edad del trabajador',
+  })
   @IsNotEmpty({ message: 'La edad no puede estar vacia' })
   @IsInt({ message: 'La edad debe ser un numero entero' })
   @Min(16, { message: 'Los tarbajadores deben ser mayores de 16 años' })
   edad: number
 
+  @ApiProperty({
+    example: '629384748',
+    description: 'Número de teléfono del trabajador',
+  })
   @IsNotEmpty({ message: 'El telefono no debe estar vacio' })
   @IsMobilePhone(
     'es-ES',
@@ -40,6 +61,10 @@ export class CreateTrabajadorDto {
   )
   telefono: string
 
+  @ApiProperty({
+    example: 'MANAGER',
+    description: 'Nombre de la categoria del trabajador',
+  })
   @IsNotEmpty({ message: 'La posicion no puede estar vacia' })
   @IsEnum(PosicionesValidas, {
     message: 'La posicion debe ser una valida, nota: es keysensitive',

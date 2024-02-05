81261f5e6d63c2a62e3b344a91bee1470f060ebc
KevinSanchez5 <@KevinSanchez5>
2024-02-05 17:26:04 +0100

    Hotfix swagger tag añadido

--- src/config/swagger/swagger.config.ts
+++ src/config/swagger/swagger.config.ts
@@ -21,6 +21,7 @@ export function setupSwagger(app: INestApplication) {
     .addTag('Auth', 'Operaciones de autenticación')
     .addTag('Posiciones', 'Operaciones con las posiciones')
     .addTag('Trabajadores', 'Operaciones con los trabajadores')
+    .addTag('Restaurantes', 'Operaciones con los trabajadores')
     .addBearerAuth()
     .build()
 
